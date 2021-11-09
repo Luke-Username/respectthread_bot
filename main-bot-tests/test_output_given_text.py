139121f5b6393aa6d9caf82f0a541a8c05b5d634
Luke-Username <venovillage@gmail.com>
2021-11-08 21:33:13 -0500

    Change test script to remove links from generated comments

--- main-bot-tests/test_output_given_text.py
+++ main-bot-tests/test_output_given_text.py
@@ -27,9 +27,23 @@ post = title + " " + f.read()
 
 character_list = mcr.search_characters(title, post, cur)
 if character_list:
-    print(replier.generate_comment(cur, character_list, False))
+    comment = replier.generate_comment(cur, character_list, False)
+    print(remove_links_from_comment(comment))
 
 # Close the cursor and connection
 cur.close()
 con.close()
 #print("Disconnected from database")
+
+# This function removes links from generated comments.
+# The point is to test that the bot knows which characters to link,
+# not if it will link a specific respect thread.
+# Doing things this way means we will rarely have to update a test case if an RT is updated.
+def remove_links_from_comment(comment: str) -> str:
+    no_link_comment = ""
+    for line in comment.split("\n"):
+        if line[0] == "-":
+            no_link_comment += "-"
+        else:
+            no_link_comment += line
+    return no_link_comment
