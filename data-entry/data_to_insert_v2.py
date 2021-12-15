35972b327c04cecae6519fd7aab082630bd5e7c8
Luke-Username <venovillage@gmail.com>
2021-12-14 22:21:57 -0500

    Update data insertion script to check for valid regex patterns on versions

--- data-entry/data_to_insert_v2.py
+++ data-entry/data_to_insert_v2.py
@@ -9,7 +9,7 @@ add_data(['Sully'],
 False,
 False,
 [
-'{"Monsters,? Inc"}'
+    ["Monsters,? Inc"]
 ],
 'Monsters, Inc.',
 '{' + '{}'.format(id) + '}'
@@ -22,12 +22,10 @@ False,
 update_respectthread(cur, 1189, 'Respect Beowulf (2007 Film)', 'https://redd.it/dspz5z')
 """
 
-import psycopg2         # Interface with PostgreSQL
+#import psycopg2         # Interface with PostgreSQL
 import re               # Regular expressions
 import sys                          # For terminal arguments and to import from different folders
 sys.path.insert(1, "../main-bot")   # This path is to import modules the bot uses. It is relative to the shell script that runs the tests.
-
-import config           # Login details
 
 name_lists = []
 default_names = []
@@ -37,6 +35,7 @@ version_lists = []
 displayed_version_names = []
 rt_id_arrays = []
 num_chars = []
+default_chars = []
 
 def get_rt_id(cur, title, link):
     query = "SELECT COUNT(*) FROM respectthread WHERE link = '{}';".format(link)
@@ -52,20 +51,41 @@ def get_rt_id(cur, title, link):
         cur.execute("SELECT id FROM respectthread WHERE link = '{}'".format(link))
         return cur.fetchone()[0];
 
+def is_valid_regex(string: str) -> bool:
+    try:
+        re.compile(string)
+        return True
+    except re.error:
+        print("WARNING: {} is not a valid regular expression!".format(string))
+        return False
+
 def add_data(name_list, default_name, is_team, is_default, version_list, displayed_version_name, rt_id_array):
     # Check if the names are valid regular expressions
-    for name in name_list:    
-        try:
-            re.compile(name)
-        except re.error:
-            print("WARNING: {} is not a valid regular expression!".format(name))
+    formatted_name_list = []
+    for name in name_list:
+        if not is_valid_regex(name):
             return
+        else:
+            # Turn the name into a string acceptable for PostgreSQL
+            formatted_name_list.append(name.replace('\\', '\\\\'))
 
-    name_lists.append(name_list)
+    formatted_version_list = []
+    for version in version_list:
+        version_array_string = '{'
+        for regex in version:
+            if not is_valid_regex(regex):
+                return
+            else:
+                version_array_string += '"{}",'.format(regex.replace('\\', '\\\\'))
+        
+        version_array_string = version_array_string.strip(',') + '}'
+        formatted_version_list.append(version_array_string)
+
+    name_lists.append(formatted_name_list)
     default_names.append(default_name)
     is_team_list.append(is_team)
     is_default_list.append(is_default)
-    version_lists.append(version_list)
+    version_lists.append(formatted_version_list)
     displayed_version_names.append(displayed_version_name)
     rt_id_arrays.append(rt_id_array)
     num_chars.append(True)
@@ -74,15 +94,6 @@ def update_respectthread(cur, id, title, link):
     cur.execute("UPDATE respectthread SET title='{}', link='{}' WHERE id={}".format(title, link, id))
     print("Successfully updated respectthread at id={}".format(id))
 
-# Opening connection to database
-con = psycopg2.connect(
-    host = config.host,
-    database = config.database,
-    user = config.d_user,
-    password = config.d_password
-)
-cur = con.cursor()
-
 ########################################
 
 add_data(['Tighten'],
@@ -90,52 +101,34 @@ add_data(['Tighten'],
 False,
 False,
 [
-r'{"Tighten vs"}', r'{"vs\\.? Tighten"}'
+    ['Tighten vs'], ['vs\.? Tighten'],
+    ['Metrocity'], ['Roxanne']
 ],
 'Megamind',
 '{1220}'
 )
 #https://www.reddit.com/r/whowouldwin/comments/rg8si3/tighten_vs_tricky_the_clown/hoinfzq/?context=3
 
+
 ########################################
 
-def insert_character_name(cur, name_list, default_name, is_team):
-    rows_inserted = 0
-    query = "INSERT INTO character_name (name, default_name, is_team) VALUES "
-    for name in name_list:
-        cur.execute("SELECT COUNT(*) FROM character_name WHERE name='{}' AND default_name='{}';".format(name, default_name))
-        num_results = cur.fetchone()
-        if num_results[0] == 0:
-            query += "('{}', '{}', '{}'),".format(name, default_name, is_team)
-            rows_inserted += 1
-
-    if rows_inserted != 0:
-        query = query.rstrip(",") + ";"
-        cur.execute(query)
-        print("Successfully inserted {} into TABLE character_name ({} rows)".format(default_name, rows_inserted))
-                
-def insert_character(cur, default_name, version_list, is_default, rt_id_array, displayed_version_name):
-    rows_inserted = 0
-    query = "INSERT INTO character (default_name, version, is_default, rt_id_array, displayed_version_name) VALUES "
-    for version in version_list:
-        cur.execute("SELECT COUNT(*) FROM character WHERE default_name='{}' AND version='{}';".format(default_name, version))
-        num_results = cur.fetchone()
-        if num_results[0] == 0:
-            query += "('{}', '{}', {}, '{}', '{}'),".format(default_name, version, is_default, rt_id_array, displayed_version_name)
-            rows_inserted += 1
-    
-    if rows_inserted != 0:
-        query = query.rstrip(",") + ";"
-        cur.execute(query)
-        print("Successfully inserted {} into TABLE character ({} rows)".format(default_name, rows_inserted))
-
 for i in range(len(num_chars)):
-    insert_character_name(cur, name_lists[i], default_names[i], is_team_list[i])
-    insert_character(cur, default_names[i], version_lists[i], is_default_list[i], rt_id_arrays[i], displayed_version_names[i])
+    print(name_lists[i])
+    print(default_names[i])
+    print(is_team_list[i])
+    print()
 
-con.commit()
-print("Committed")
+    print(default_names[i])
+    print(version_lists[i])
+    print(is_default_list[i])
+    print(rt_id_arrays[i])
+    print(displayed_version_names[i])
+    print()
 
-# Close the cursor and connection
-cur.close()
-con.close()
+    if is_default_list[i]:
+        default_chars.append(default_names[i])
+
+if len(default_chars) > 0:
+    print('These characters are set to default. Please double check this is correct:')
+    for character in default_chars:
+        print(character)
