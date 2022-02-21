6077167fd3bbd15fe7011742982184c9c6df0f2d
Luke-Username <venovillage@gmail.com>
2022-02-21 10:52:39 -0500

    Add data corrections

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,31 @@ cur = con.cursor()
 
 ########################################
 
+add_data(['Storm'],
+'Storm',
+False,
+False,
+[
+    ['vs\.? Storm', 'Johnny Storm'], ['vs\.? Storm', 'Hulk']
+],
+'616',
+'{2386}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/sxo8hd/she_hulk_vs_wonder_woman_vs_terminator_x_vs_storm/hxtdglm/?context=3
+
+########################################
+
+add_data(['Terminator X'],
+'Terminator X',
+False,
+True,
+[
+    ['Terminatrix']
+],
+'',
+'{328}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/sxo8hd/she_hulk_vs_wonder_woman_vs_terminator_x_vs_storm/hxtdglm/?context=3
 
 ########################################
 
