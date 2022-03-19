88ec98be96d0f226fd7970319acca30403de99dd
Luke-Username <venovillage@gmail.com>
2022-03-18 22:36:20 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,10 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 4750, 'Respect Jack the Ripper! (Fate)', 'https://redd.it/tf22xj')
+
+########################################
+
 add_data(['Kazuma'],
 'Kazuma',
 False,
@@ -118,6 +122,126 @@ False,
 '{3785}'
 )
 #https://www.reddit.com/r/whowouldwin/comments/tf376t/kazuma_vs_subaru_natsuki/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Koulin The Instigator (Avatar: The Kyoshi Novels)', 'https://redd.it/tfjb6r')
+add_data(['Koulin'],
+'Koulin',
+False,
+False,
+[
+    ['Avatar'], ['Kyoshi'], ['Instigator']
+],
+'Avatar: TLA',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tfjb6r/respect_koulin_the_instigator_avatar_the_kyoshi/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Vibranium (Marvel Cinematic Universe)', 'https://redd.it/tfjmxd')
+add_data(['Vibranium'],
+'Vibranium',
+False,
+False,
+[
+    ['Marvel Cinematic Universe'], ['MCU']
+],
+'MCU',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tfjmxd/respect_vibranium_marvel_cinematic_universe/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Reptile! (Mortal Kombat 2021)', 'https://redd.it/tg5c2y')
+add_data(['Reptile'],
+'Reptile',
+False,
+False,
+[
+    ['Mortal Kombat.*2021']
+],
+'Mortal Kombat, 2021',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tg5c2y/respect_reptile_mortal_kombat_2021/
+
+########################################
+
+id = get_rt_id(cur, 'Respect SCP-2521, ●●|●●●●●|●●|● (SCP Foundation)', 'https://redd.it/tgkasd')
+add_data(['SCP ?(-| )? ?2521'],
+'SCP-2521',
+False,
+False,
+[
+    ['SCP Foundation']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tgkasd/respect_scp2521_scp_foundation/
+
+########################################
+
+id = get_rt_id(cur, '[NSFW] Respect The Sorcerer Hunters! (Sorcerer Hunters)', 'https://redd.it/tgw7ah')
+add_data(['Sorcerer Hunters'],
+'Sorcerer Hunters',
+True,
+True,
+[
+    ['Sorcerer Hunters ?\(Sorcerer Hunters']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tgw7ah/nsfw_respect_the_sorcerer_hunters_sorcerer_hunters/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Kid Gladiator (Marvel, Earth-616)', 'https://redd.it/tgx0mp')
+add_data(['Kid Gladiator'],
+'Kid Gladiator',
+False,
+True,
+[
+    ['616']
+],
+'616',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/tgx0mp/respect_kid_gladiator_marvel_earth616/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Judge Doom (Who Framed Roger Rabbit?)', 'https://redd.it/th17kp')
+add_data(['Judge Doom'],
+'Judge Doom',
+False,
+True,
+[
+    ['Roger', 'Rabbit?']
+],
+'Who Framed Roger Rabbit?',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/th17kp/respect_judge_doom_who_framed_roger_rabbit/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Dan Hibiki! (Udon Comics Street Fighter)', 'https://redd.it/th2pgl')
+add_data(['Dan Hibiki'],
+'Dan Hibiki',
+False,
+False,
+[
+    ['UDON']
+],
+'UDON',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/th2pgl/respect_dan_hibiki_udon_comics_street_fighter/
 
 ########################################
 
