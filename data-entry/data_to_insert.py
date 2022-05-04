d62375a57a44f11b3227455804e7bae1cc3a8d1d
Luke-Username <venovillage@gmail.com>
2022-05-03 20:40:41 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,167 @@ cur = con.cursor()
 
 ########################################
 
+add_data(['Constantine'],
+'John Constantine',
+False,
+True,
+[
+    ['\(DC( Comics)?\)'], ['\[DC( Comics)?\]']
+],
+'DC',
+'{1704}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/uhk9ro/lex_luther_batman_and_john_constantine_vs_doctor/i76hlbx/?context=3
+
+########################################
+
+id = get_rt_id(cur, 'Red Skull (616) feat repository', 'https://redd.it/kuyb0k')
+add_data(['Red Skulls?'],
+'Red Skull',
+False,
+True,
+[
+    ['616']
+],
+'616',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/WhoWouldWinWorkshop/comments/kuyb0k/red_skull_616_feat_repository/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Tintin (The Adventures of Tintin: The Secret of the Unicorn)', 'https://redd.it/ugo6sr')
+add_data(['Tintin'],
+'Tintin',
+False,
+False,
+[
+    ['Adventures of Tintin', 'Secret of.*Unicorn']
+],
+'The Adventures of Tintin: The Secret of the Unicorn',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/ugo6sr/respect_tintin_the_adventures_of_tintin_the/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Captain Archibald Haddock (The Adventures of Tintin: The Secret of the Unicorn)', 'https://redd.it/ugo71w')
+add_data(['(Captain|Archibald) Haddock'],
+'Captain Haddock',
+False,
+False,
+[
+    ['Adventures of Tintin', 'Secret of.*Unicorn']
+],
+'The Adventures of Tintin: The Secret of the Unicorn',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/ugo71w/respect_captain_archibald_haddock_the_adventures/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Revka Scyros III, Psycho-Man (Marvel, Earth-1610)', 'https://redd.it/ugq2sx')
+add_data(['Psycho(-| )?Man'],
+'Psycho-Man',
+False,
+False,
+[
+    ['1610']
+],
+'1610',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/ugq2sx/respect_revka_scyros_iii_psychoman_marvel/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Hanami! (Jujutsu Kaisen)', 'https://redd.it/ugqxkv')
+add_data(['Hanami'],
+'Hanami',
+False,
+False,
+[
+    ['Jujus?t?s?u Kaisen'], ['JJK']
+],
+'Jujutsu Kaisen',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/ugqxkv/respect_hanami_jujutsu_kaisen/
+
+########################################
+
+id = get_rt_id(cur, 'Respect: He-Man! (Pre-Crisis DC Comics)', 'https://redd.it/ugwrf3')
+add_data(['He(-| )?Man'],
+'He-Man',
+False,
+False,
+[
+    ['Pre(-| )?Crisis']
+],
+'Pre-Crisis',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/ugwrf3/respect_heman_precrisis_dc_comics/
+
+########################################
+
+id = get_rt_id(cur, 'Respect King Elizabello II (One Piece)', 'https://redd.it/uh4uq7')
+add_data(['Elizabello IIs?'],
+'Elizabello II',
+False,
+True,
+[
+    ['One ?Piece?']
+],
+'One Piece',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/uh4uq7/respect_king_elizabello_ii_one_piece/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Secret Society of Second-Born Royals (The Secret Society of Second-Born Royals)', 'https://redd.it/uhcj6v')
+add_data(['Secret Society of Second(-| )Born Royals'],
+'Secret Society of Second-Born Royals',
+False,
+True,
+[
+    ['Prince(ss)?']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/uhcj6v/respect_the_secret_society_of_secondborn_royals/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Optimus Prime! (Transformers: Shattered Glass)', 'https://redd.it/uhh0rh')
+add_data(['Optimus Prime'],
+'Optimus Prime',
+False,
+False,
+[
+    ['Shattered Glass']
+],
+'Transformers: Shattered Glass',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/uhh0rh/respect_optimus_prime_transformers_shattered_glass/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Glory (Buffy the Vampire Slayer)', 'https://redd.it/uhpujz')
+add_data(['Glory'],
+'Glory',
+False,
+False,
+[
+    ['Buffy the Vampire Slayer'],['Buffyverse'], ['Buffy', 'Angel']
+],
+'Buffy the Vampire Slayer',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/uhpujz/respect_glory_buffy_the_vampire_slayer/
 
 ########################################
 
