efaf82fef38ca5e9eef77c41ea3e87077ff15d3f
Luke-Username <venovillage@gmail.com>
2022-07-15 16:43:22 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,230 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 8143, 'Respect Narushima Koga, The Fist Eye! (Kengan Omega)', 'https://redd.it/vyg6u5')
+
+########################################
+
+id = get_rt_id(cur, 'Respect Kushala, the Spirit Rider (Marvel, 616)', 'https://redd.it/vxoohy')
+add_data(['Kushala'],
+'Kushala',
+False,
+False,
+[
+    ['616'], ['Spirit Rider'], ['Marvel']
+],
+'616',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vxoohy/respect_kushala_the_spirit_rider_marvel_616/
+
+########################################
+
+id = get_rt_id(cur, 'Respect America Chavez (Marvel Cinematic Universe)', 'https://redd.it/vyudsa')
+add_data(['America Chavez'],
+'America Chavez',
+False,
+False,
+[
+    ['Marvel Cinematic Universe'], ['MCU'], ['Doctor Strange 2']
+],
+'MCU',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vyudsa/respect_america_chavez_marvel_cinematic_universe/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Strength of a River in His Shoulders (The Dresden Files)', 'https://redd.it/vxtba3')
+add_data(['Strength of a River in His Shoulders'],
+'Strength of a River in His Shoulders',
+False,
+True,
+[
+    ['Dresden']
+],
+'The Dresden Files',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vxtba3/respect_strength_of_a_river_in_his_shoulders_the/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Warden Carlos Ramirez (The Dresden Files)', 'https://redd.it/vyan2m')
+add_data(['Carlos Ramirez'],
+'Carlos Ramirez',
+False,
+False,
+[
+    ['Dresden'], ['Warden']
+],
+'The Dresden Files',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vyan2m/respect_warden_carlos_ramirez_the_dresden_files/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Blackstaff, Ebenezer McCoy (The Dresden Files)', 'https://redd.it/vz17w4')
+add_data(['Ebenez(a|e)r McCoy'],
+'Ebenezar McCoy',
+False,
+True,
+[
+    ['Dresden']
+],
+'The Dresden Files',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vz17w4/respect_the_blackstaff_ebenezer_mccoy_the_dresden/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ivy, the Archive (The Dresden Files)', 'https://redd.it/vz4sft')
+add_data(['Ivy'],
+'Ivy',
+False,
+False,
+[
+    ['Dresden Files'], ['The Archive']
+],
+'The Dresden Files',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vz4sft/respect_ivy_the_archive_the_dresden_files/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Sha Wujing (Xi Xing Ji)', 'https://redd.it/vy0ndl')
+add_data(['Sha Wujing'],
+'Sha Wujing',
+False,
+False,
+[
+    ['Xi Xing Ji']
+],
+'Xi Xing Ji',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vy0ndl/respect_sha_wujing_xi_xing_ji/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Sun Wukong (Xi Xing Ji)', 'https://redd.it/vyu5bj')
+add_data(['Sun Wu ?Kong'],
+'Sun Wukong',
+False,
+False,
+[
+    ['Xi Xing Ji']
+],
+'Xi Xing Ji',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vyu5bj/respect_sun_wukong_xi_xing_ji/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Echidna, the Witch of Greed (Re:Zero, Anime)', 'https://redd.it/vy2tpy')
+add_data(['Echidna'],
+'Echidna',
+False,
+False,
+[
+    ['Re:? ?Zero']
+],
+'Re:Zero',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vy2tpy/respect_echidna_the_witch_of_greed_rezero_anime/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Satella, the Witch of Envy (Re:Zero, Anime)', 'https://redd.it/vyvdhe')
+add_data(['Satella'],
+'Satella',
+False,
+True,
+[
+    ['Re:? ?Zero']
+],
+'Re:Zero',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vyvdhe/respect_satella_the_witch_of_envy_rezero_anime/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Izou (One Piece)', 'https://redd.it/vye1hx')
+add_data(['Izou'],
+'Izou',
+False,
+False,
+[
+    ['One ?Piece?']
+],
+'One Piece',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vye1hx/respect_izou_one_piece/
+
+add_data(['Izo'],
+'Izo',
+False,
+False,
+[
+    ['One ?Piece?']
+],
+'One Piece',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vye1hx/respect_izou_one_piece/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Skye Ryland! (Five Kingdoms)', 'https://redd.it/vyey72')
+add_data(['Skye Ryland'],
+'Skye Ryland',
+False,
+False,
+[
+    ['Five Kingdoms']
+],
+'Five Kingdoms',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vyey72/respect_skye_ryland_five_kingdoms/
+
+########################################
+
+id = get_rt_id(cur, 'Respect SCP-2241 (SCP Foundation)', 'https://redd.it/vzioby')
+add_data(['SCP ?(-| )? ?2241'],
+'SCP-2241',
+False,
+True,
+[
+    ['SCP Foundation']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vzioby/respect_scp2241_scp_foundation/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Eleven (Stranger Things)', 'https://redd.it/vzst6f')
+add_data(['Eleven'],
+'Eleven',
+False,
+False,
+[
+    ['Stranger Things']
+],
+'Stranger Things',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/vzst6f/respect_eleven_stranger_things/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
