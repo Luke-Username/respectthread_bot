00de512967998bf16449d23ddc6ddf8238365ab0
Luke-Username <venovillage@gmail.com>
2022-11-23 19:37:46 -0500

    Add data corrections

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,253 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 5653, 'Respect 2B (Nier: Automata)', 'https://redd.it/z1k28f')
+update_respectthread(cur, 6029, 'Respect: Magnus the Red (Warhammer 40k)', 'https://redd.it/z1r7ra')
+update_respectthread(cur, 8598, 'Respect Bor Burison, The Father of The All-Father (Marvel, 616)', 'https://redd.it/z1ydyk')
+
+########################################
+
+add_data(['Heroe?s? Association'],
+'Hero Association',
+False,
+False,
+[
+    ['My Hero Academia'], ['\(My Hero\)'], ['(M|BN?)HA'], ['Boku no Hero']
+],
+'My Hero Academia',
+'{}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/z143b6/madara_naruto_shippuden_vs_hero_association_mha/ix8tkss/?context=3
+
+########################################
+
+add_data(['Thor'],
+'Thor',
+False,
+False,
+[
+    ['God of War Thor']
+],
+'God of War',
+'{}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/z1xwui/thor_replaces_kratos_which_god_can_stop_him/ixgbjfc/?context=3
+
+add_data(['Thor'],
+'Thor',
+False,
+False,
+[
+    ['God of War', 'Pantheons?']
+],
+'God of War',
+'{}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/z2myy3/god_of_war_war_of_the_pantheons/ixh364y/?context=3
+
+########################################
+
+add_data(['Nemesis'],
+'Nemesis',
+False,
+False,
+[
+    ['Nemesis ?\(RE']
+],
+'Resident Evil',
+'{5373}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/z28x8t/nemisis_re_vs_bruce_wayne_arkham/ixf4tbb/?context=3
+
+########################################
+
+id = get_rt_id(cur, 'Respect Cynthia (Pokemon Anime)', 'https://redd.it/z0zplp')
+add_data(['Cynthia'],
+'Cynthia',
+False,
+False,
+[
+    ['Pok(e|é)m(o|a)n']
+],
+'Pokémon',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z0zplp/respect_cynthia_pokemon_anime/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Kendra Sorenson! (Fablehaven)', 'https://redd.it/z19ia5')
+add_data(['Kendra Sorenson'],
+'Kendra Sorenson',
+False,
+True,
+[
+    ['Fablehaven']
+],
+'Fablehaven',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z19ia5/respect_kendra_sorenson_fablehaven/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Jack "The Ripper" Hanma (Grappler Baki)', 'https://redd.it/z1daxc')
+add_data(['Jack Hanma'],
+'Jack Hanma',
+False,
+True,
+[
+    ['Baki(verse)?']
+],
+'Baki',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1daxc/respect_jack_the_ripper_hanma_grappler_baki/
+
+add_data(['Jack'],
+'Jack',
+False,
+False,
+[
+    ['Hanma (Family|Bloodline)'], ['Jack ?\(Baki']
+],
+'Baki',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1daxc/respect_jack_the_ripper_hanma_grappler_baki/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Mr. T (Anki Deck #1635663511)', 'https://redd.it/z1ppe2')
+add_data(['Mr\.? T'],
+'Mr. T',
+False,
+False,
+[
+    ['Anki Deck']
+],
+'Anki Deck',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1ppe2/respect_mr_t_anki_deck_1635663511/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Sega Shiro! (Go Sega 60th anniversary adverts)', 'https://redd.it/z1s00o')
+add_data(['Sega Shiro'],
+'Sega Shiro',
+False,
+True,
+[
+    ['advert(isement)s?']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1s00o/respect_sega_shiro_go_sega_60th_anniversary/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Zala / Miss Doublefinger (One Piece)', 'https://redd.it/z1xkv9')
+add_data(['M(is)?s\.? Doublefinger'],
+'Miss Doublefinger',
+False,
+True,
+[
+    ['One ?Piece?']
+],
+'One Piece',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1xkv9/respect_zala_miss_doublefinger_one_piece/
+
+add_data(['Zala'],
+'Zala',
+False,
+False,
+[
+    ['One ?Piece?']
+],
+'One Piece',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1xkv9/respect_zala_miss_doublefinger_one_piece/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Odin! (Fate)', 'https://redd.it/z1zw00')
+add_data(['Odin'],
+'Odin',
+False,
+False,
+[
+    ['Odin ?\(Fate']
+],
+'Fate',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z1zw00/respect_odin_fate/
+
+########################################
+
+id = get_rt_id(cur, "Respect William! (The Inquisitor''s Tale: Or, The Three Magical Children and Their Holy Dog)", 'https://redd.it/z292x1')
+add_data(['William'],
+'William',
+False,
+False,
+[
+    ["Inquisitor\'\'?s Tale"]
+],
+"The Inquisitor''s Tale",
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Crane (The Sting of Death)', 'https://redd.it/z29geo')
+add_data(['Crane'],
+'Crane',
+False,
+False,
+[
+    ['Sting of Death']
+],
+'The Sting of Death',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z29geo/respect_crane_the_sting_of_death/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Rogu (American Dad!)', 'https://redd.it/z2plym')
+add_data(['Rogu'],
+'Rogu',
+False,
+False,
+[
+    ['American Dad']
+],
+'American Dad!',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z2plym/respect_rogu_american_dad/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Riful of the West and Dauf (Claymore)', 'https://redd.it/z33l41')
+add_data(['Riful'],
+'Riful',
+False,
+False,
+[
+    ['Claymore']
+],
+'Claymore',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/z33l41/respect_riful_of_the_west_and_dauf_claymore/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
