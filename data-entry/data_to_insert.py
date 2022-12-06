30bc640457b96255fdb5558c2924e8d784e91492
Luke-Username <venovillage@gmail.com>
2022-12-05 23:34:31 -0500

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,439 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 591, "Respect Venom (Sony''s Spider-Man Universe)", 'https://redd.it/zbiboj')
+update_respectthread(cur, 5735, "Respect Kenny (Telltale''s The Walking Dead Game)", 'https://redd.it/zbwl44')
+
+########################################
+
+id = get_rt_id(cur, 'Respect Thor, The Destroyer (God of War)', 'https://redd.it/zdcr1j')
+add_data(['Thor'],
+'Thor',
+False,
+False,
+[
+    ['God of War']
+],
+'God of War',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/zbffpw/god_of_war_ragnarok_thor_vs_kratos_arm_wrestle/iyrgyv2/?context=3
+
+########################################
+
+add_data(['Betsy Braddock'],
+'Betsy Braddock',
+False,
+True,
+[
+    ['616']
+],
+'616',
+'{2380}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/zb530m/captain_britain_betsy_braddock_vs_magik_616/iyqiqfl/?context=3
+
+########################################
+
+id = get_rt_id(cur, 'Respect Manny (Swiss Army Man)', 'https://redd.it/zayl6i')
+add_data(['Manny'],
+'Manny',
+False,
+False,
+[
+    ['Swiss Army Man']
+],
+'Swiss Army Man',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zayl6i/respect_manny_swiss_army_man/
+
+########################################
+
+id = get_rt_id(cur, 'Respect: Phantom Superman! (Pre-Crisis DC Comics)', 'https://redd.it/zb0uek')
+add_data(['Super(-| )?man'],
+'Phantom Superman',
+False,
+False,
+[
+    ['Pre(-| )?Crisis']
+],
+'Pre-Crisis',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zayl6i/respect_manny_swiss_army_man/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Air Man! (Mega Man)', 'https://redd.it/zb21wq')
+add_data(['Air Man'],
+'Air Man',
+False,
+False,
+[
+    ['Mega ?Man'], ['Air ?man ga Taosenai']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Yellow Devil (Mega Man)', 'https://redd.it/zb4f4t')
+add_data(['Yellow Devil'],
+'Yellow Devil',
+False,
+True,
+[
+    ['Mega ?Man']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zb4f4t/respect_the_yellow_devil_mega_man/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Guts Man! (Mega Man)', 'https://redd.it/zc319r')
+add_data(['Guts Man'],
+'Guts Man',
+False,
+True,
+[
+    ['Mega ?Man']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zc319r/respect_guts_man_mega_man/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Needle Man (Mega Man)', 'https://redd.it/zco44s')
+add_data(['Needle Man'],
+'Needle Man',
+False,
+True,
+[
+    ['Mega ?Man']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Terra! (Mega Man V)', 'https://redd.it/zcrz8t')
+add_data(['Terra'],
+'Terra',
+False,
+False,
+[
+    ['Mega ?Man']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcrz8t/respect_terra_mega_man_v/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Sunstar! (Mega Man V)', 'https://redd.it/zcs91t')
+add_data(['Sunstar'],
+'Sunstar',
+False,
+False,
+[
+    ['Mega ?Man']
+],
+'Mega Man',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcs91t/respect_sunstar_mega_man_v/
+
+########################################
+
+id = get_rt_id(cur, "Respect Riot (Sony''s Spider-Man Universe)", 'https://redd.it/zbicjk')
+add_data(['Riot'],
+'Riot',
+False,
+False,
+[
+    ['Riot ?\(Sonys?'], ['Carnage', '\(Sony\)']
+],
+'Sony',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbicjk/respect_riot_sonys_spiderman_universe/
+
+########################################
+
+id = get_rt_id(cur, "Respect Carnage (Sony''s Spider-Man Universe)", 'https://redd.it/zbid67')
+add_data(['Carnage'],
+'Carnage',
+False,
+False,
+[
+    ['Carnage ?\(Sonys?'], ['Riot', '\(Sony\)'], ['Let There Be Carnage'], ['Venom 2'], ['LTBC']
+],
+'Sony',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbid67/respect_carnage_sonys_spiderman_universe/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Frances Barrison, Shriek (Venom: Let There Be Carnage)', 'https://redd.it/zcaloa')
+add_data(['Frances Barrison'],
+'Frances Barrison',
+False,
+False,
+[
+    ['Sonys?']
+],
+'Sony',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcaloa/respect_frances_barrison_shriek_venom_let_there/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Mori Nagayoshi! (Fate)', 'https://redd.it/zbqssj')
+add_data(['Mori Nagayoshi'],
+'Mori Nagayoshi',
+False,
+True,
+[
+    ['Fate ?verse']
+],
+'Fate',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbqssj/respect_mori_nagayoshi_fate/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Seven Ghosts (Claymore)', 'https://redd.it/zbr5ax')
+add_data(['(Seven|7) Ghosts'],
+'Seven Ghosts',
+True,
+False,
+[
+    ['Claymore']
+],
+'Claymore',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbr5ax/respect_the_seven_ghosts_claymore/
+
+########################################
+
+id = get_rt_id(cur, "Respect Spa''ark, the Death Ranger! (Mighty Morphin'' Power Rangers [BOOM! Studios])", 'https://redd.it/zbt4x1')
+add_data(["Spa''ark"],
+"Spa''ark",
+False,
+True,
+[
+    ['Power Rangers?'], ['BOOM!? Studios?']
+],
+'Power Rangers',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbt4x1/respect_spaark_the_death_ranger_mighty_morphin/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Naruo (Speedboy!)', 'https://redd.it/zbvnqs')
+add_data(['Naruo'],
+'Naruo',
+False,
+False,
+[
+    ['Speedboy']
+],
+'Speedboy!',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zbvnqs/respect_naruo_speedboy/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Balls (Speedboy!)', 'https://redd.it/zbvo8k')
+add_data(['Balls'],
+'Balls',
+False,
+False,
+[
+    ['Speedboy']
+],
+'Speedboy!',
+'{' + '{}'.format(id) + '}'
+)
+
+########################################
+
+id = get_rt_id(cur, 'Respect Bugsy (Pokemon Anime)', 'https://redd.it/zc9hk6')
+add_data(['Bugsy'],
+'Bugsy',
+False,
+False,
+[
+    ['Pok(e|é)m(o|a)n']
+],
+'Pokémon',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zc9hk6/respect_bugsy_pokemon_anime/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Captain Smiley (Comic Jumper)', 'https://redd.it/zcah87')
+add_data(['Captain Smiley'],
+'Captain Smiley',
+False,
+True,
+[
+    ['Comic Jumper']
+],
+'Comic Jumper',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcah87/respect_captain_smiley_comic_jumper/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Sukapon (Super Smash Bros.)', 'https://redd.it/zcm83r')
+add_data(['Sukapon'],
+'Sukapon',
+False,
+False,
+[
+    ['Smash (Bro(ther)?s?|Ultimate)']
+],
+'Super Smash Bros',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcm83r/respect_sukapon_super_smash_bros/
+
+########################################
+
+id = get_rt_id(cur, 'Respect John Constantine (Constantine 2005)', 'https://redd.it/zcv919')
+add_data(['John Constantine'],
+'John Constantine',
+False,
+False,
+[
+    ['2005']
+],
+'2005',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zcv919/respect_john_constantine_constantine_2005/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Herald (Fortnite)', 'https://redd.it/zcvi8x')
+add_data(['Herald'],
+'Herald',
+False,
+False,
+[
+    ['Fortnite']
+],
+'Fortnite',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Martial Artist (Sifu)', 'https://redd.it/zd77ay')
+add_data(['Martial Artist'],
+'Martial Artist',
+False,
+False,
+[
+    ['Sifu']
+],
+'Sifu',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zd77ay/respect_the_martial_artist_sifu/
+
+add_data(['Sifu'],
+'Sifu',
+False,
+False,
+[
+    ['Sifu ?\(Sifu'], ['Sifu protagonist']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zd77ay/respect_the_martial_artist_sifu/
+
+add_data(['The Child'],
+'The Child',
+False,
+False,
+[
+    ['Sifu']
+],
+'Sifu',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zd77ay/respect_the_martial_artist_sifu/
+
+
+########################################
+
+id = get_rt_id(cur, 'Respect: Smash! (Image Comics)', 'https://redd.it/zdakm4')
+add_data(['Smash'],
+'Smash',
+False,
+False,
+[
+    ['Image Comics']
+],
+'Image Comics',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zdakm4/respect_smash_image_comics/
+
+
+########################################
+
+id = get_rt_id(cur, 'Respect Django! (Django)', 'https://redd.it/zdm8nh')
+add_data(['Django'],
+'Django',
+False,
+True,
+[
+    ['Django ?\(Django']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zdm8nh/respect_django_django/
+
+
+########################################
+
+id = get_rt_id(cur, 'Respect Orphan (Young Justice)', 'https://redd.it/zdobjd')
+add_data(['Orphan'],
+'Orphan',
+False,
+False,
+[
+    ['Young Justice']
+],
+'Young Justice',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zdobjd/respect_orphan_young_justice/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
