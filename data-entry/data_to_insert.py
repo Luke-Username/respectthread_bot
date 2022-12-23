3182b2f42d90414bbeee4bea0f86459c4e1dfa7e
Luke-Username <venovillage@gmail.com>
2022-12-23 08:35:01 -0500

    Add data corrections

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,49 @@ cur = con.cursor()
 
 ########################################
 
+id = get_rt_id(cur, 'Respect the Ninth Doctor (Doctor Who)', 'https://redd.it/zsqwiv')
+add_data(['(Nin|9)th Doctor'],
+'The Ninth Doctor',
+False,
+True,
+[
+    ['(Doctor|Dr\.?) ?Who'], ['Who(ni)?verse']
+],
+'Doctor Who',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zsqwiv/respect_the_ninth_doctor_doctor_who/
+
+########################################
+
+id = get_rt_id(cur, 'Respect: Black Adam! (Batman: The Brave and the Bold)', 'https://redd.it/zsvzqm')
+add_data(['Black Adam'],
+'Black Adam',
+False,
+False,
+[
+    ['Brave (and|&) the Bold']
+],
+'The Brave and the Bold',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zsvzqm/respect_black_adam_batman_the_brave_and_the_bold/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Honkai Beasts (Honkai Impact 3rd)', 'https://redd.it/zt2ngu')
+add_data(['Honkai Beasts?'],
+'Honkai Beasts',
+False,
+True,
+[
+    ['Honkai Impact']
+],
+'Honkai Impact 3rd',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/zt2ngu/respect_honkai_beasts_honkai_impact_3rd/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
