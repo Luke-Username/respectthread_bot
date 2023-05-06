59f0004382c1ea2f1414d397fd9762cd31fd5dd5
Luke-Username <venovillage@gmail.com>
2023-05-06 19:23:41 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,24 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 21136, 'Respect Pirate Queen Tagaka (Avatar: The Kyoshi Novels)', 'https://redd.it/139sv21')
+
+########################################
+
+id = get_rt_id(cur, 'Respect Kurako / Heavenly Star☆Angelica (Nocturne of the Heaven) [Nijisanji Kamigakari campaign]', 'https://redd.it/139l39i')
+add_data(['Kurako'],
+'Kurako',
+False,
+False,
+[
+    ['Nocturne of the Heaven'], ['NOTH'], ['Kamigakari'], ['Nijisanji'],
+    ['Saiyou Kurako']
+],
+'Nocturne of the Heaven',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/139l39i/respect_kurako_heavenly_starangelica_nocturne_of/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
