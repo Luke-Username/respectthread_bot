180332a985f6c11a9a50de5a3bcb483230ffba2c
Luke-Username <venovillage@gmail.com>
2023-10-30 07:31:51 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,10 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 6020, "[Warhammer 40k] New respect thread for the C''tan", 'https://redd.it/17hw9hc')
+
+########################################
+
 id = get_rt_id(cur, 'Respect Officer Roger Mortis (Dead Heat)', 'https://redd.it/17hp0s2')
 add_data(['Roger Mortis'],
 'Roger Mortis',
@@ -134,6 +138,246 @@ False,
 '{' + '{}'.format(id) + '}'
 )
 #https://www.reddit.com/r/respectthreads/comments/17httlx/respect_medusa_conan_the_adventurer/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Rouge the Bat (Speed Smackdown)', 'https://redd.it/17i3wjw')
+add_data(['Ro(ug|gu)e the Bat'],
+'Rouge the Bat',
+False,
+False,
+[
+    ['Speed Smackdown']
+],
+'Speed Smackdown',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17i3wjw/respect_rouge_the_bat_speed_smackdown/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Catwoman (Speed Smackdown)', 'https://redd.it/17i3wlo')
+add_data(['Cat(-| )?woman'],
+'Catwoman',
+False,
+False,
+[
+    ['Speed Smackdown']
+],
+'Speed Smackdown',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17i3wlo/respect_catwoman_speed_smackdown/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Mort (Discworld)', 'https://redd.it/17idzr1')
+add_data(['Mort'],
+'Mort',
+False,
+False,
+[
+    ['Discworld']
+],
+'Discworld',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17idzr1/respect_mort_discworld/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Red Death (Masque of the Red Death)', 'https://redd.it/17ietvq')
+add_data(['Red Death'],
+'Red Death',
+False,
+False,
+[
+    ['Masque of the Red Death']
+],
+'Masque of the Red Death',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17ietvq/respect_the_red_death_masque_of_the_red_death/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Chihiro Rokuhira (Kagurabachi)', 'https://redd.it/17ifba8')
+add_data(['Chihiro Rokuhira'],
+'Chihiro Rokuhira',
+False,
+True,
+[
+    ['Kagura ?bachi']
+],
+'Kagurabachi',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17ifba8/respect_chihiro_rokuhira_kagurabachi/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ruin! (Undead Unluck)', 'https://redd.it/17io55o')
+add_data(['Ruin'],
+'Ruin',
+False,
+False,
+[
+    ['Undead Unluck']
+],
+'Undead Unluck',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17io55o/respect_ruin_undead_unluck/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ruin! (Undead Unluck)', 'https://redd.it/17io55o')
+add_data(['Ruin'],
+'Ruin',
+False,
+False,
+[
+    ['Undead Unluck']
+],
+'Undead Unluck',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Super X (Godzilla Franchise, Heisei Continuity)', 'https://redd.it/17iuftr')
+add_data(['Super X'],
+'Super X',
+False,
+False,
+[
+    ['Heisei'], ['Godzilla']
+],
+'Godzilla',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17iuftr/respect_the_super_x_godzilla_franchise_heisei/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Godzilla Junior (Godzilla Franchise, Heisei Continuity)', 'https://redd.it/17ivxb2')
+add_data(['Godzilla Junior'],
+'Godzilla Junior',
+False,
+True,
+[
+    ['Heisei']
+],
+'Godzilla',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17ivxb2/respect_godzilla_junior_godzilla_franchise_heisei/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Golem (Der Golem)', 'https://redd.it/17j3ohm')
+add_data(['Der Golem'],
+'Der Golem',
+False,
+True,
+[
+    ['How He Came into the World']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17j3ohm/respect_the_golem_der_golem/
+
+########################################
+
+id = get_rt_id(cur, 'Respect IG-88 (Star Wars)', 'https://redd.it/17j6fse')
+add_data(['IG(-| )88'],
+'IG-88',
+False,
+True,
+[
+    ['S(tar )?Wars']
+],
+'Star Wars',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Damned Thing! (The Damned Thing)', 'https://redd.it/17j7ztd')
+add_data(['The Damned Thing'],
+'The Damned Thing',
+False,
+True,
+[
+    ['The Damned Thing ?\(The Damned Thing\)'], ['1893']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17j7ztd/respect_the_damned_thing_the_damned_thing/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Garlic Jr! (Dragon Ball Z)', 'https://redd.it/17j9bfj')
+add_data(['Garlic Jr'],
+'Garlic Jr',
+False,
+True,
+[
+    ['Dragon ?Ball'], ['DB(Z|S)']
+],
+'Dragon Ball',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17j9bfj/respect_garlic_jr_dragon_ball_z/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Red Ronin (Marvel Comics, Earth-616)', 'https://redd.it/17jkhhd')
+add_data(['Red Ronin'],
+'Red Ronin',
+False,
+False,
+[
+    ['616'], ['\(Marvel\)']
+],
+'616',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17jkhhd/respect_red_ronin_marvel_comics_earth616/
+
+########################################
+
+id = get_rt_id(cur, "Respect Golden Boy (Amazon''s The Boys: Gen V)", 'https://redd.it/17jlj76')
+add_data(['Golden Boy'],
+'Golden Boy',
+False,
+False,
+[
+    ['The Boys'], ['Gen V']
+],
+'The Boys',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17jlj76/respect_golden_boy_amazons_the_boys_gen_v/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Lasser Glass! (Oculus)', 'https://redd.it/17jm3fa')
+add_data(['Lasser Glass'],
+'Lasser Glass',
+False,
+False,
+[
+    ['Oculus']
+],
+'Oculus',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/17jm3fa/respect_the_lasser_glass_oculus/
 
 ########################################
 
