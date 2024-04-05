fc3cb21e651f0cb435d401c94aa26ac6aae92f7c
Luke-Username <venovillage@gmail.com>
2024-04-05 10:37:21 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -121,6 +121,162 @@ False,
 
 ########################################
 
+add_data(['Snake Eyes'],
+'Snake Eyes',
+False,
+False,
+[
+    ['G\.?I\.? Joe', 'Movies?']
+],
+'G.I. Joe Movies',
+'{442}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/1bvvosz/snake_eyes_vs_the_shredder_gi_joe_vs_tmnt/ky21hzi/?context=3
+
+########################################
+
+id = get_rt_id(cur, "Respect Aura the Guillotine (Frieren: Beyond Journey''s End)", 'https://redd.it/1bufyy9')
+add_data(['Aura'],
+'Aura',
+False,
+False,
+[
+    ['Aura ?\(Frieren\)'], ['Aura the Guillotine']
+],
+"Frieren: Beyond Journey''s End",
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bufyy9/respect_aura_the_guillotine_frieren_beyond/
+
+########################################
+
+id = get_rt_id(cur, 'Respect BJ Blazkowicz (Wolfenstein) (id Software)', 'https://redd.it/1btzs97')
+id2 = get_rt_id(cur, 'Respect BJ Blazkowicz (Wolfenstein) (Activision)', 'https://redd.it/1burg1m')
+id3 = get_rt_id(cur, 'Respect BJ Blazkowicz (Wolfenstein RPG)', 'https://redd.it/1burgn5')
+id4 = get_rt_id(cur, 'Respect BJ Blazkowicz (Wolfenstein) (MachineGames)', 'https://redd.it/1bvlgps')
+id5 = get_rt_id(cur, 'Respect BJ Blazkowicz (Quake Champions)', 'https://redd.it/1btzstm')
+add_data(['B\.?J\.? Blazkowicz'],
+'B.J. Blazkowicz',
+False,
+True,
+[
+    ['Wolfenstein']
+],
+'Wolfenstein',
+'{' + '{}, {}, {}, {}, {}'.format(id, id2, id3, id4, id5) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1btzs97/respect_bj_blazkowicz_wolfenstein_id_software/
+#https://www.reddit.com/r/respectthreads/comments/1burg1m/respect_bj_blazkowicz_wolfenstein_activision/
+#https://www.reddit.com/r/respectthreads/comments/1burgn5/respect_bj_blazkowicz_wolfenstein_rpg/
+#https://www.reddit.com/r/respectthreads/comments/1bvlgps/respect_bj_blazkowicz_wolfenstein_machinegames/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Raya (Raya and the Last Dragon)', 'https://redd.it/1burvpx')
+add_data(['Raya'],
+'Raya',
+False,
+False,
+[
+    ['Raya and the Last Dragon']
+],
+'Raya and the Last Dragon',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1burvpx/respect_raya_raya_and_the_last_dragon/
+
+########################################
+
+id = get_rt_id(cur, 'Respect X-50, "Aaron Stack" (Marvel, 616)', 'https://redd.it/1bv0x96')
+add_data(['X-50'],
+'X-50',
+False,
+False,
+[
+    ['Aaron Stack', '616']
+],
+'616',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bv0x96/respect_x50_aaron_stack_marvel_616/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Troglor (Smiling Friends)', 'https://redd.it/1bv0yiw')
+add_data(['Troglor'],
+'Troglor',
+False,
+False,
+[
+    ['Smiling Friends']
+],
+'Smiling Friends',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bv0yiw/respect_troglor_smiling_friends/
+
+########################################
+
+id = get_rt_id(cur, 'Respect James (Smiling Friends)', 'https://redd.it/1bv12jy')
+add_data(['James'],
+'James',
+False,
+False,
+[
+    ['Smiling Friends']
+],
+'Smiling Friends',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bv12jy/respect_james_smiling_friends/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Anti-Pops (Regular Show)', 'https://redd.it/1bwiqf4')
+add_data(['Anti(-| )Pops'],
+'Anti-Pops',
+False,
+True,
+[
+    ['Regular Show']
+],
+'Regular Show',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bwiqf4/respect_antipops_regular_show/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Angstrom Levy (Image Comics)', 'https://redd.it/1bwiqgf')
+add_data(['Angstrom Levy'],
+'Angstrom Levy',
+False,
+True,
+[
+    ['Image Comics']
+],
+'Image Comics',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bwiqgf/respect_angstrom_levy_image_comics/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Psycrow (Earthworm Jim)', 'https://redd.it/1bwiqha')
+add_data(['Psy-?Crow'],
+'Psy-Crow',
+False,
+True,
+[
+    ['Earthworm Jim']
+],
+'Earthworm Jim',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1bwiqha/respect_psycrow_earthworm_jim/
+
+########################################
+
 def insert_character_name(cur, name_list, default_name, is_team):
     rows_inserted = 0
     query = "INSERT INTO character_name (name, default_name, is_team) VALUES "
