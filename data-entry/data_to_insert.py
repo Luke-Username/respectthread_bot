da38a1c2c60eecec47977628d87e9c0b814b7fbe
Luke-Username <venovillage@gmail.com>
2024-10-10 15:43:22 -0400

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,309 @@ cur = con.cursor()
 
 ########################################
 
+add_data(['Chucky'],
+'Chucky',
+False,
+False,
+[
+    ['Monokuma', 'Chucky vs|vs\.? Chucky']
+],
+"Child''s Play",
+'{13094}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/1g0n9uu/monokuma_vs_chucky/lr9y4ux/?context=3
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Great God Cay, the Brontosaurus (Beyond the Great South Wall)', 'https://redd.it/1fzjlke')
+add_data(['Brontosaurus'],
+'Brontosaurus',
+False,
+False,
+[
+    ['Beyond the Great South Wall']
+],
+'Beyond the Great South Wall',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzjlke/respect_the_great_god_cay_the_brontosaurus_beyond/
+
+add_data(['Cay'],
+'Cay',
+False,
+False,
+[
+    ['Beyond the Great South Wall']
+],
+'Beyond the Great South Wall',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzjlke/respect_the_great_god_cay_the_brontosaurus_beyond/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Butcher (Emesis Blue)', 'https://redd.it/1fzqnif')
+add_data(['Butcher'],
+'Butcher',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzqnif/respect_the_butcher_emesis_blue/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Stalingrad (Emesis Blue)', 'https://redd.it/1fzrwk1')
+add_data(['Stalingrad'],
+'Stalingrad',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzrwk1/respect_stalingrad_emesis_blue/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Detective (Emesis Blue)', 'https://redd.it/1fzrzvd')
+add_data(['Detective'],
+'Detective',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzrzvd/respect_the_detective_emesis_blue/
+
+add_data(['Jacques Murnau'],
+'Jacques Murnau',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzrzvd/respect_the_detective_emesis_blue/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Soldier (Emesis Blue)', 'https://redd.it/1fzsdbq')
+add_data(['Soldier'],
+'Soldier',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzsdbq/respect_the_soldier_emesis_blue/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Dr. Fritz Ludwig (Emesis Blue)', 'https://redd.it/1fzsdwd')
+add_data(['Dr\.? Fritz Ludwig'],
+'Dr. Fritz Ludwig',
+False,
+False,
+[
+    ['Emesis Blue']
+],
+'Emesis Blue',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzsdwd/respect_dr_fritz_ludwig_emesis_blue/
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Headless Horseman (The Legend of Sleepy Hollow)', 'https://redd.it/1fzr94o')
+add_data(['Headless Horseman'],
+'Headless Horseman',
+False,
+False,
+[
+    ['Legend of Sleepy Hollow']
+],
+'The Legend of Sleepy Hollow',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzr94o/respect_the_headless_horseman_the_legend_of/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ruby Knowby (Ash vs Evil Dead)', 'https://redd.it/1fzr9r7')
+add_data(['Ruby Knowby'],
+'Ruby Knowby',
+False,
+True,
+[
+    ['Ash vs\.? Evil Dead']
+],
+'Ash vs Evil Dead',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzr9r7/respect_ruby_knowby_ash_vs_evil_dead/
+
+add_data(['Ruby Knowby'],
+'Ruby Knowby',
+False,
+False,
+[
+    ['Ash vs\.? Evil Dead']
+],
+'Ash vs Evil Dead',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzr9r7/respect_ruby_knowby_ash_vs_evil_dead/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ash Williams (Evil Dead: The Musical)', 'https://redd.it/1g0oqr4')
+add_data(['Ash Williams'],
+'Ash Williams',
+False,
+False,
+[
+    ['Evil Dead:? The Musical']
+],
+'Evil Dead: The Musical',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1g0oqr4/respect_ash_williams_evil_dead_the_musical/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Necronomicon (Evil Dead: The Musical)', 'https://redd.it/1g0oqaa')
+add_data(['Necronomicon'],
+'Necronomicon',
+False,
+False,
+[
+    ['Evil Dead:? The Musical']
+],
+'Evil Dead: The Musical',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1g0oqaa/respect_the_necronomicon_evil_dead_the_musical/
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Unnamed Demon that Possessed Marcus (Ash vs Evil Dead)', 'https://redd.it/1fzsieu')
+add_data(['Demon that Possessed Marcus'],
+'Demon that Possessed Marcus',
+False,
+False,
+[
+    ['Ash vs\.? Evil Dead']
+],
+'Ash vs Evil Dead',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzsieu/respect_the_unnamed_demon_that_possessed_marcus/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Senro Grumer! (Bakauge Sentai BoonBoomger)', 'https://redd.it/1fzrihc')
+add_data(['Senro Grumer'],
+'Senro Grumer',
+False,
+False,
+[
+    ['Bakauge Sentai BoonBoomger']
+],
+'Bakauge Sentai BoonBoomger',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzrihc/respect_senro_grumer_bakauge_sentai_boonboomger/
+
+########################################
+
+id = get_rt_id(cur, "Respect James'' Weezing (Pokemon Anime)", 'https://redd.it/1g0jhkp')
+add_data(['Weezing'],
+'Weezing',
+False,
+False,
+[
+    ['James']
+],
+'James',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1g0jhkp/respect_james_weezing_pokemon_anime/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Han & his Den of Thieves (Kung Fu Panda)', 'https://redd.it/1fzy8wa')
+add_data(['Han'],
+'Han',
+False,
+False,
+[
+    ['Han.*Kung Fu Panda'], ['Han the Pangolin']
+],
+'Kung Fu Panda',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1fzy8wa/respect_han_his_den_of_thieves_kung_fu_panda/
+
+########################################
+
+id = get_rt_id(cur, "Respect Kino (Kino''s Journey —the Beautiful World—, 2003)", 'https://redd.it/1fzzwg0')
+add_data(['Kino'],
+'Kino',
+False,
+False,
+[
+    ["Kino''s Journey"]
+],
+"Kino''s Journey",
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Partridge Creek Monster (The Monster of "Partridge Creek")', 'https://redd.it/1g0h5x0')
+add_data(['Monster'],
+'Monster',
+False,
+False,
+[
+    ['Partridge Creek']
+],
+'The Monster of "Partridge Creek"',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1g0h5x0/respect_the_partridge_creek_monster_the_monster/
+
+########################################
+
+id = get_rt_id(cur, 'Respect Reverend Lester Lowe, The Werewolf (Cycle of the Werewolf)', 'https://redd.it/1g0hlio')
+add_data(['Lester Lowe'],
+'Lester Lowe',
+False,
+False,
+[
+    ['Cycle of the Werewolf']
+],
+'Cycle of the Werewolf',
+'{' + '{}'.format(id) + '}'
+)
+#https://www.reddit.com/r/respectthreads/comments/1g0hlio/respect_reverend_lester_lowe_the_werewolf_cycle/
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
