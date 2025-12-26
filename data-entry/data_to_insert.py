2edc0bd4fb60c5f3a7b88487c3d27e2de1c1778e
Luke-Username <venovillage@gmail.com>
2025-12-26 11:10:14 -0500

    Add new RT data

--- data-entry/data_to_insert.py
+++ data-entry/data_to_insert.py
@@ -107,6 +107,287 @@ cur = con.cursor()
 
 ########################################
 
+update_respectthread(cur, 5434, 'Respect Rosalina (Super Mario)', 'https://www.reddit.com/r/respectthreads/comments/1ptrsxn/respect_rosalina_super_mario/')
+update_respectthread(cur, 12403, 'Respect Merus (Dragon Ball Super)', 'https://www.reddit.com/r/respectthreads/comments/1pup8tr/respect_merus_dragon_ball_super/')
+update_respectthread(cur, 5252, 'Respect Solid Snake (Metal Gear)', 'https://www.reddit.com/r/respectthreads/comments/1putrjj/respect_solid_snake_metal_gear/')
+update_respectthread(cur, 5244, 'Respect Gray Fox (Metal Gear)', 'https://www.reddit.com/r/respectthreads/comments/1pv0vsy/respect_gray_fox_metal_gear/')
+
+########################################
+
+add_data(['Wolverine'],
+'Wolverine',
+False,
+False,
+[
+    ['Batman.*Wolverine|Wolverine.*Batman', 'vs']
+],
+'616',
+'{2391}'
+)
+#https://www.reddit.com/r/whowouldwin/comments/1pv12bz/batman_vs_wolverine/nvst6np/?context=3
+
+########################################
+
+id = get_rt_id(cur, 'Respect Chance! (Forsaken)', 'https://www.reddit.com/r/respectthreads/comments/1ptmx3b/respect_chance_forsaken/')
+add_data(['Chance'],
+'Chance',
+False,
+False,
+[
+    ['Chance ?\(Forsaken\)']
+],
+'Forsaken',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Dante the Great (V/H/S: Viral)', 'https://www.reddit.com/r/respectthreads/comments/1ptv5c5/respect_dante_the_great_vhs_viral/')
+add_data(['Dante the Great'],
+'Dante the Great',
+False,
+False,
+[
+    ['V/?H/?S:? Viral']
+],
+'V/H/S: Viral',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Ality Mellowlink (Armor Hunter Mellowlink)', 'https://www.reddit.com/r/respectthreads/comments/1pu3u45/respect_ality_mellowlink_armor_hunter_mellowlink/')
+add_data(['Ality Mellowlink'],
+'Ality Mellowlink',
+False,
+True,
+[
+    ['Armor Hunter Mellowlink']
+],
+'Armor Hunter Mellowlink',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Joe 90! (Joe 90)', 'https://www.reddit.com/r/respectthreads/comments/1pu8ovj/respect_joe_90_joe_90/')
+add_data(['Joe 90'],
+'Joe 90',
+False,
+True,
+[
+    ['Joe 90 ?\(Joe 90']
+],
+'',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Pipo Snake (Mesal Gear Solid: Snake Escape)', 'https://www.reddit.com/r/respectthreads/comments/1pv0vti/respect_pipo_snake_mesal_gear_solid_snake_escape/')
+add_data(['Pipo Snake'],
+'Pipo Snake',
+False,
+False,
+[
+    ['Mesal Gear']
+],
+'Mesal Gear Solid: Snake Escape',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Road Runner (Looney Tunes)', 'https://www.reddit.com/r/respectthreads/comments/1pv21k3/respect_road_runner_looney_tunes/')
+add_data(['Road Runner'],
+'Road Runner',
+False,
+True,
+[
+    ['Looney Tunes']
+],
+'Looney Tunes',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Ghosts of Christmas (A Christmas Carol)', 'https://www.reddit.com/r/respectthreads/comments/1pvdik0/respect_the_ghosts_of_christmas_a_christmas_carol/')
+add_data(['Ghosts'],
+'Ghosts',
+True,
+False,
+[
+    ['A Christmas Carol']
+],
+'A Christmas Carol',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Ghosts? of Christmas'],
+'Ghosts of Christmas',
+True,
+True,
+[
+    ['A Christmas Carol']
+],
+'A Christmas Carol',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['(Three|3) Ghosts'],
+'Three Ghosts',
+True,
+False,
+[
+    ['Christmas']
+],
+'A Christmas Carol',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect The Christmas Trees (Treevenge)', 'https://www.reddit.com/r/respectthreads/comments/1pve0g8/respect_the_christmas_trees_treevenge/')
+add_data(['Christmas Trees?'],
+'Christmas Trees',
+False,
+False,
+[
+    ['Treevenge']
+],
+'Treevenge',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+
+########################################
+
+id = get_rt_id(cur, "Respect ''Vulture'' Li Yao (Swallowed Star)", 'https://www.reddit.com/r/respectthreads/comments/1pvsf8a/respect_vulture_li_yao_swallowed_star/')
+add_data(['Vulture'],
+'Vulture',
+False,
+False,
+[
+    ['Swallowed Star']
+],
+'Swallowed Star',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Li Yao'],
+'Li Yao',
+False,
+False,
+[
+    ['Swallowed Star']
+],
+'Swallowed Star',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect the Winged Lion (Delicious in Dungeon)', 'https://www.reddit.com/r/respectthreads/comments/1pvvrw7/respect_the_winged_lion_delicious_in_dungeon/')
+add_data(['Winged Lion'],
+'Winged Lion',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+########################################
+
+id = get_rt_id(cur, 'Respect Laios, Marcille, Senshi, Izutsumi, and Chilchuck (Delicious in Dungeon)', 'https://www.reddit.com/r/respectthreads/comments/1pvvtda/respect_laios_marcille_senshi_izutsumi_and/')
+add_data(["Laios's? Party"],
+"Laios'' Party",
+True,
+True,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Laios'],
+'Laios',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Marcille'],
+'Marcille',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Senshi'],
+'Senshi',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Izutsumi'],
+'Izutsumi',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
+add_data(['Chilchuck'],
+'Chilchuck',
+False,
+False,
+[
+    ['Delicious in Dungeon'], ['Dungeon Meshi']
+],
+'Delicious in Dungeon',
+'{' + '{}'.format(id) + '}'
+)
+#
+
 ########################################
 
 def insert_character_name(cur, name_list, default_name, is_team):
