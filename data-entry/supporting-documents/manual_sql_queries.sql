013b67e292f8c7a4920d35be1da179eabf254b3d
Luke-Username <venovillage@gmail.com>
2026-02-03 12:05:16 -0500

    Insert data

--- data-entry/supporting-documents/manual_sql_queries.sql
+++ data-entry/supporting-documents/manual_sql_queries.sql
@@ -1,11 +1,4 @@
-UPDATE character_name SET name='Preston( J)?\.? Cole' WHERE name='Preston( J)?\\.? Cole';
-UPDATE character_name SET name='M(iste)?r\.? Popo' WHERE name='M(iste)?r\\.? Popo';
-UPDATE character_name SET name='George H\.? W\.? Bush' WHERE name='George H\\.? W\\.? Bush';
-UPDATE character_name SET name='George W\.? Bush' WHERE name='George W\\.? Bush';
-UPDATE character_name SET name='Evangeline A\.? ?K\.? McDowell' WHERE name='Evangeline A\\.? ?K\\.? McDowell';
-UPDATE character_name SET name='Dr\.? Namba' WHERE name='Dr\\.? Namba';
+--https://stackoverflow.com/questions/9736085/run-a-postgresql-sql-file-using-command-line-arguments
+sudo -u postgres psql -d respectthreads -f manual_sql_queries.sql
 
-########################################
-
-INSERT INTO name_conflict (name, conflict, first_char) VALUES ('Tony', 'Tony Soprano', 0);
-INSERT INTO name_conflict (name, conflict, first_char) VALUES ('Super(-| )?man', 'Superman 2025', 0);
+--########################################
