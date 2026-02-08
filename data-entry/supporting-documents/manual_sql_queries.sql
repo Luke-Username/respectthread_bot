f795a253f0e17d4e7fb76d159f5ae610935a888c
Luke-Username <venovillage@gmail.com>
2026-02-08 17:25:12 -0500

    Add data corrections

--- data-entry/supporting-documents/manual_sql_queries.sql
+++ data-entry/supporting-documents/manual_sql_queries.sql
@@ -2,3 +2,62 @@
 sudo -u postgres psql -d respectthreads -f manual_sql_queries.sql
 
 --########################################
+
+UPDATE character_name SET name='Takamichi T\.? Takahata' WHERE name='Takamichi T\\.? Takahata';
+UPDATE character_name SET name='Takane D\.? Goodman' WHERE name='Takamichi T\\.? Takahata';
+UPDATE character_name SET name='(Mount|Mt\.?) Lady' WHERE name='(Mount|Mt\\.?) Lady';
+UPDATE character_name SET name='Donatello K\.? Davis' WHERE name='Donatello K\\.? Davis';
+UPDATE character_name SET name='Mr\.? Mime' WHERE name='Donatello K\\.? Davis';
+UPDATE character_name SET name='Lt\.? Surge' WHERE name='Lt\\.? Surge';
+UPDATE character_name SET name='Mr\.? A' WHERE name='Mr\\.? A';
+UPDATE character_name SET name='(Monster|Kaiju) (#|No)\.? ?8' WHERE name='(Monster|Kaiju) (#|No)\\.? ?8';
+UPDATE character_name SET name='Mr\.? Hinx' WHERE name='Mr\\.? Hinx';
+UPDATE character_name SET name='(Doctor|Dr\.?) Aris' WHERE name='(Doctor|Dr\\.?) Aris';
+UPDATE character_name SET name='Regis K\.? Landegre' WHERE name='Regis K\\.? Landegre';
+UPDATE character_name SET name='Seira J\.? Loyard' WHERE name='Seira J\\.? Loyard';
+UPDATE character_name SET name='Monkey D\.? Garp' WHERE name='Monkey D\\.? Garp'
+UPDATE character_name SET name='Trafalgar( D\.? Water)? Law' WHERE name='Trafalgar( D\\.? Water)? Law';
+UPDATE character_name SET name='(Doctor|Dr\.?) Aphra' WHERE name='(Doctor|Dr\\.?) Aphra';
+UPDATE character_name SET name='(Doctor|Dr)\.? Doom' WHERE name='(Doctor|Dr)\\.? Doom';
+UPDATE character_name SET name='Dr\.? O''? ?Ryen' WHERE name='Dr\\.? O''? ?Ryen';
+UPDATE character_name SET name='P\.?R\.?E\.?T\.?Z\.?L' WHERE name='P\\.?R\\.?E\\.?T\\.?Z\\.?L';
+UPDATE character_name SET name='Mr?s\.? Mowz' WHERE name='Mr?s\\.? Mowz';
+UPDATE character_name SET name='(Doctor|Dr\.?) Bong' WHERE name='(Doctor|Dr\\.?) Bong';
+UPDATE character_name SET name='(Mr\.?|Mister) Electric' WHERE name='(Mr\\.?|Mister) Electric';
+UPDATE character_name SET name='(Mr\.?|Mister) Nimbus' WHERE name='(Mr\\.?|Mister) Nimbus';
+UPDATE character_name SET name='John( F\.)? Walker' WHERE name='John( F\\.)? Walker';
+UPDATE character_name SET name='(Doctor|Dr\.?) Mike Morgan' WHERE name='(Doctor|Dr\\.?) Mike Morgan';
+UPDATE character_name SET name='(Doctor|Dr\.?) Mary Michaels' WHERE name='(Doctor|Dr\\.?) Mary Michaels';
+UPDATE character_name SET name='(Doctor|Dr\.?) Stuart Lowe' WHERE name='(Doctor|Dr\\.?) Stuart Lowe';
+UPDATE character_name SET name='(Doctor|Dr\.?) (Byron )?Orpheus' WHERE name='(Doctor|Dr\\.?) (Byron )?Orpheus';
+UPDATE character_name SET name='(Mister|Mr\.?) Bliss' WHERE name='(Mister|Mr\\.?) Bliss';
+UPDATE character_name SET name='(Doctor|Dr\.?) Druid' WHERE name='(Doctor|Dr\\.?) Druid';
+UPDATE character_name SET name='Melty Q\.? Melromarc' WHERE name='Melty Q\\.? Melromarc';
+UPDATE character_name SET name='Notorious B\.?I\.?G' WHERE name='Notorious B\\.?I\\.?G';
+UPDATE character_name SET name='Aurora E\.? Juutilainen' WHERE name='Aurora E\\.? Juutilainen';
+UPDATE character_name SET name='Sanya( V\.?)? Litvyak' WHERE name='Sanya( V\\.?)? Litvyak';
+UPDATE character_name SET name='Joseph( G(ustav)?\.?)? Newton' WHERE name='Joseph( G(ustav)?\\.?)? Newton';
+UPDATE character_name SET name='Marcos( E(ringrad)?\.?)? Garcia' WHERE name='Marcos( E(ringrad)?\\.?)? Garcia';
+UPDATE character_name SET name='E\.?G\.? Mine' WHERE name='E\\.?G\\.? Mine';
+UPDATE character_name SET name='M\.?D\.? Geist' WHERE name='M\\.?D\\.? Geist';
+UPDATE character_name SET name='M\.?O\.?N' WHERE name='M\\.?O\\.?N';
+UPDATE character_name SET name='Mr?s\.? Smith' WHERE name='Mr?s\\.? Smith';
+UPDATE character_name SET name='(N\.?|Nefarious) Tropy' WHERE name='(N\\.?|Nefarious) Tropy';
+UPDATE character_name SET name='King K\.? ?Rool' WHERE name='King K\\.? ?Rool';
+UPDATE character_name SET name='B(lack)?\.? Orchid' WHERE name='B(lack)?\\.? Orchid';
+UPDATE character_name SET name='Plants vs\.? Zombies' WHERE name='Plants vs\\.? Zombies';
+UPDATE character_name SET name='F\.?A\.?N\.?G' WHERE name='F\\.?A\\.?N\\.?G';
+UPDATE character_name SET name='R(ainbow)?\.? ?Mika' WHERE name='R(ainbow)?\\.? ?Mika';
+UPDATE character_name SET name='Wendy( O\.?)? Koopa' WHERE name='Wendy( O\\.?)? Koopa';
+UPDATE character_name SET name='Dr\.? Crygor' WHERE name='Dr\\.? Crygor';
+UPDATE character_name SET name='R\.?O\.?B' WHERE name='R\\.?O\\.?B';
+UPDATE character_name SET name='M(is)?s\.? Pauling' WHERE name='M(is)?s\\.? Pauling';
+UPDATE character_name SET name='A\.?B\.?E' WHERE name='A\\.?B\\.?E';
+
+
+
+
+
+
+
+
